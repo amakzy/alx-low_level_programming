6d59fbc3a14282c0287895d5a029b4ee5876356f
amakzy <ahmadamakxy128@gmail.com>
2023-02-21 07:39:44 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	int i;
-	char p[] = "_putchar ";
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	for (i = 0; i < 9; i++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-	_putchar(p[i]);
+		_putchar(str[count]);
 	}
 	_putchar('\n');
 	return (0);
 	}
-
