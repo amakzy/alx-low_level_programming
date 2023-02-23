59b7c10965ab369028a9023023e4cf1c3df45db2
amakzy <ahmadamakxy128@gmail.com>
2023-02-23 11:00:35 -0800

    advanced

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,18 +13,17 @@ void print_number(int n)
 	{
 		n1 = -n;
 		_putchar('-');
-		{
-			else
-			{
-				n1 = n;
-			}
-
-			if (n1 / 10)
-			{
-				print_number(n1 / 10);
-			}
-
-			_putchar((n1 % 10) + '0');
-		}
 	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+
+	_putchar((n1 % 10) + '0');
 }
