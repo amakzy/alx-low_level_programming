e14f9bae7ae432a55aec39a9f6354ea99a256389
amakzy <ahmadamakxy128@gmail.com>
2023-03-20 15:10:32 -0700

    task mandatory and advanced

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -23,6 +18,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
