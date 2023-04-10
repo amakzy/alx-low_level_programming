7a5e5a5eae39fa4cf95b0e763c06acf45751efee
amakzy <ahmadamakxy128@gmail.com>
2023-04-09 23:47:41 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,16 +14,16 @@
 
 void _error(int e, char *filename)
 {
-if (e == 98)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-exit(98);
-}
-if (e == 99)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-exit(99);
-}      
+	if (e == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (e == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
 
 /**
@@ -35,41 +35,41 @@ exit(99);
 
 void cp(char *file_from, char *file_to)
 {
-int fd_read, res_read, fd_write, res_write;
-char *buf[1024];
+	int fd_read, res_read, fd_write, res_write;
+	char *buf[1024];
 /* READ */
-fd_read = open(file_from, O_RDONLY);
-if (fd_read < 0)
-_error(98, file_from);
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read < 0)
+		_error(98, file_from);
 /* WRITE */
-fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd_write < 0)
-{
-close(fd_read);
-_error(99, file_to);
-}
-do {
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_error(99, file_to);
+	}
+	do {
 /* READ */
-res_read = read(fd_read, buf, 1024);
-if (res_read < 0)
-_error(98, file_from);
+		res_read = read(fd_read, buf, 1024);
+	if (res_read < 0)
+		_error(98, file_from);
 /* WRITE */
-res_write = write(fd_write, buf, res_read);
-if (res_write < res_read)
-_error(99, file_to);
-}
-while (res_write == 1024);
-if (close(fd_read) < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
-close(fd_write);
-exit(100);
-}
-if (close(fd_write) < 0)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
-exit(100);
-}
+	res_write = write(fd_write, buf, res_read);
+	if (res_write < res_read)
+		_error(99, file_to);
+	} while (res_write == 1024);
+
+	if (close(fd_read) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		close(fd_write);
+		exit(100);
+	}
+	if (close(fd_write) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
 }
 
 /**
@@ -81,11 +81,12 @@ exit(100);
 
 int main(int ac, char *av[])
 {
-if (ac != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-cp(av[1], av[2]);
-return (0);
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	cp(av[1], av[2]);
+
+	return (0);
 }
